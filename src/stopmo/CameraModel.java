5f9adb1e780aa3d9f00a7b9925bbe47c3e8a441a
valery <vbrasseur@gmail.com>
2015-05-20 10:22:20 +0200

    remove old websocket references

--- src/stopmo/CameraModel.java
+++ src/stopmo/CameraModel.java
@@ -38,7 +38,6 @@ public class CameraModel implements Runnable {
 		state = 0;
 	}
 
-	
 
 	@Override
 	public void run() {
@@ -58,88 +57,88 @@ public class CameraModel implements Runnable {
 			e.printStackTrace();
 		}
 	}
+
+public void parseMessage(String msg) {		
+	if (msg.startsWith("hello")) {
+		model = msg.split(":",2)[1];
+	} 
+	if (msg.startsWith("PSize")) {
+		getPreviewSize(msg);
+	}
+	if (msg.startsWith("Shot")) {
+		System.out.println("new shot");
+		state = 2;
+	}
+	if (msg.startsWith("Balance")) {
+		parseBalance(msg);
+	} 
+	if (msg.startsWith("Preview")) {
+		System.out.println("new preview");
+		state = 1;
+	} 
+	System.out.println("parseMessage: " + msg);
 }
 
-	public void parseMessage(String msg) {		
-		if (msg.startsWith("hello")) {
-			model = msg.split(":",2)[1];
-		} 
-		if (msg.startsWith("PSize")) {
-			getPreviewSize(msg);
+/*
+ * camera send preview as w,h
+ */
+private void getPreviewSize(String msg) {
+	String blist = msg.split(":",2)[1];
+	String[] slist = blist.split("\\s*,\\s*");
+	width = Integer.parseInt(slist[0]);
+	height = Integer.parseInt(slist[1]);
+}
+
+/*
+ * camera send the Balance mode list
+ * store it
+ */
+private void parseBalance(String msg) {
+	String blist = msg.split(":",2)[1];
+	WhiteBalanceList = Arrays.asList(blist.split("\\s*,\\s*"));		
+}
+
+public List<String> getWhiteBalanceList() {
+	return WhiteBalanceList;
+}
+
+public void parseMessage( ByteBuffer message ) {
+	System.out.println("parseMessage: bytes" );
+
+	if (state == 2) {
+		System.out.println("parseMessage: in shot" );
+		try {
+			InputStream in = new ByteArrayInputStream(message.array());
+			BufferedImage img = ImageIO.read(in);
+
+			if (listener != null)
+				listener.onShot(img);
+
+		} catch (IOException e) {			
+			e.printStackTrace();
 		}
-		if (msg.startsWith("Shot")) {
-			System.out.println("new shot");
-			state = 2;
-		}
-		if (msg.startsWith("Balance")) {
-			parseBalance(msg);
-		} 
-		if (msg.startsWith("Preview")) {
-			System.out.println("new preview");
-			state = 1;
-		} 
-		System.out.println("parseMessage: " + msg);
+	} else if (state == 1) {
+		System.out.println("parseMessage: in preview ("+width+","+height+")" );
+
+		//YuvImage img = new YuvImage(message, ImageFormat.NV21, PreviewSizeWidth, PreviewSizeHeight, null);
 	}
+}
 
-	/*
-	 * camera send preview as w,h
-	 */
-	private void getPreviewSize(String msg) {
-		String blist = msg.split(":",2)[1];
-		String[] slist = blist.split("\\s*,\\s*");
-		width = Integer.parseInt(slist[0]);
-		height = Integer.parseInt(slist[1]);
-	}
+public void takeShot() {
+	System.out.println("take shot");
+	//TODO: use socket here !
+	//conn.send("takeShot");
+}
 
-	/*
-	 * camera send the Balance mode list
-	 * store it
-	 */
-	private void parseBalance(String msg) {
-		String blist = msg.split(":",2)[1];
-		WhiteBalanceList = Arrays.asList(blist.split("\\s*,\\s*"));		
-	}
+public String getModel() {
+	return model;
+}
 
-	public List<String> getWhiteBalanceList() {
-		return WhiteBalanceList;
-	}
+public void attach(CameraModelListener listener) {
+	this.listener = listener;
+}
 
-	public void parseMessage( ByteBuffer message ) {
-		System.out.println("parseMessage: bytes" );
-
-		if (state == 2) {
-			System.out.println("parseMessage: in shot" );
-			try {
-				InputStream in = new ByteArrayInputStream(message.array());
-				BufferedImage img = ImageIO.read(in);
-
-				if (listener != null)
-					listener.onShot(img);
-
-			} catch (IOException e) {			
-				e.printStackTrace();
-			}
-		} else if (state == 1) {
-			System.out.println("parseMessage: in preview ("+width+","+height+")" );
-
-			//YuvImage img = new YuvImage(message, ImageFormat.NV21, PreviewSizeWidth, PreviewSizeHeight, null);
-		}
-	}
-
-	public void takeShot() {
-		System.out.println("take shot");
-		conn.send("takeShot");
-	}
-
-	public String getModel() {
-		return model;
-	}
-
-	public void attach(CameraModelListener listener) {
-		this.listener = listener;
-	}
-
-	public void detach(CameraModelListener listener) {
-		this.listener = null;
-	}
+public void detach(CameraModelListener listener) {
+	this.listener = null;
+}
 }
